d4745a3bd9e9aea4a20e0fcbfa56d7c29ff9050a
Md. Ashraf Uddin <Asifashraf276@gmail.com>
2024-06-12 00:58:05 +0100

    Loader & Message part added into homescreen and product details screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -17,20 +17,20 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route index={true} path="/" element={<HomeScreen />} />
-			<Route path="/product/:id" element={<ProductScreen />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router}></RouterProvider>
-		</Provider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  </React.StrictMode>
 );
 reportWebVitals();
