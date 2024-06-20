75ca8a40342b263d265b74cfe6d8c14e9a8b573f
Md. Ashraf Uddin <Asifashraf276@gmail.com>
2024-06-20 02:58:21 +0100

    User routes and controllers are setup for backend authentication

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Add the notFound middleware
 app.use(notFound);
