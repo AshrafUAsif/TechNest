901481b93d28a3411cb1511d9ae5c1e1f9cb0b52
Md. Ashraf Uddin <Asifashraf276@gmail.com>
2024-06-27 15:40:41 +0100

    private route is setup for the shipping page.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -13,6 +13,7 @@ import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import PrivateRoute from "./components/PrivateRoute";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
@@ -21,24 +22,26 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/product/:id" element={<ProductScreen />} />
-      <Route path="/cart" element={<CartScreen />} />
-      <Route path='/login' element={<LoginScreen />} />
-      <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index={true} path="/" element={<HomeScreen />} />
+			<Route path="/product/:id" element={<ProductScreen />} />
+			<Route path="/cart" element={<CartScreen />} />
+			<Route path="/login" element={<LoginScreen />} />
+			<Route path="/register" element={<RegisterScreen />} />
+			<Route path="" element={<PrivateRoute></PrivateRoute>}>
+				<Route path="/shipping" element={<ShippingScreen />} />
+			</Route>
+		</Route>
+	)
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router}></RouterProvider>
+		</Provider>
+	</React.StrictMode>
 );
 reportWebVitals();
