428ce10f4c0182e7de95437c60c641836d8acfff
Md. Ashraf Uddin <Asifashraf276@gmail.com>
2024-07-06 17:31:03 +0100

    Fixing some problems

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
+// Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -41,14 +38,25 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID}));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 const __dirname = path.resolve(); // Set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/upload')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-
-// Add the notFound middleware
+// Error Handling Middleware
 app.use(notFound);
-
-// Add the errorHandler middleware
 app.use(errorHandler);
 
 // Unhandled promise rejections
--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -14,9 +14,9 @@ const authSlice = createSlice({
     },
     logout: (state, action) => {
       state.userInfo = null;
-      localStorage.removeItem('userInfo');
-    }
-  }
+      localStorage.clear();
+    },
+  },
 });
 
 export const { setCredentials , logout} = authSlice.actions;
